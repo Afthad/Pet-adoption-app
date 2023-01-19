6143c8fddee41101ade508746bd50fd5de8d6990
Afthad <afthadabie@gmail.com>
2023-01-19 07:10:41 +0530

    added changes

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,6 @@ class MyApp extends StatelessWidget {
       themeMode: ThemeMode.system,
       initialRoute: '/homeScreen',
    getPages: AppRouter.routes,
- // specify the fallback locale in case an invalid locale is selected.
 );
   }
 }
